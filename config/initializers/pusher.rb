d01fc15622c7e6395426ef7aa02087b5b45c355a
Andrew Paulson <andrew.l.paulson@gmail.com>
2016-04-28 15:25:16 -0700

    playing with figaro

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,7 @@
 require 'pusher'
 
-Pusher.app_id = Figaro.env.PUSHER_APP_ID
-Pusher.key = Figaro.env.PUSHER_KEY
-Pusher.secret = Figaro.env.PUSHER_SECRET
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
